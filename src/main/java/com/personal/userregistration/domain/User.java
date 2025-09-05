b63d014ca5313c1999955b9d741fefbed910116a
Julia Welter <140450028+juliawel@users.noreply.github.com>
2025-09-05 08:38:56 -0300

    feat: implement basic User domain class with validations

---
+++ src/main/java/com/personal/userregistration/domain/User.java
@@ -0,0 +1,46 @@
+package com.personal.userregistration.domain;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="user")
+public class User {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @Column(name="name", nullable = false)
+    @Size(min=2, max=60)
+    @NotBlank
+    private String name;
+
+    @Column(name="email", unique=true, nullable=false)
+    @Email(message="Please provide a valid email address.")
+    @NotBlank(message="Email is required.")
+    private String email;
+
+    @Column(name="password", nullable=false)
+    @JsonProperty(access=JsonProperty.Access.WRITE_ONLY)
+    @Size(min=8, max=20, message="Password must contain at least 8 characters.")
+    private String password;
+
+    @Column(name="birthdate", nullable=false)
+    @Past
+    private LocalDate birthdate;
+
+}
