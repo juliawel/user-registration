0e384e6f2561776fa3db6e035413b9ed9368dad2
Julia Welter <140450028+juliawel@users.noreply.github.com>
2025-09-05 10:31:54 -0300

    feat(user): add new findByEmail and deleteByEmail methods

--- src/main/java/com/personal/userregistration/infrastructure/repository/UserRepository.java
+++ src/main/java/com/personal/userregistration/infrastructure/repository/UserRepository.java
@@ -1,9 +1,15 @@
 package com.personal.userregistration.infrastructure.repository;
 
 import com.personal.userregistration.infrastructure.domain.User;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByEmail(String email);
+
+    @Transactional
+    void deleteByEmail(String email);
 }
