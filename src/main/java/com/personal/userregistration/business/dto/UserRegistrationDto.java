f307fe1f421e866b358859ead44dabf63dfd72b9
Julia Welter <140450028+juliawel@users.noreply.github.com>
2025-09-08 11:17:55 -0300

    refactor: Add @ToString to UserRegistrationDto

--- src/main/java/com/personal/userregistration/business/dto/UserRegistrationDto.java
+++ src/main/java/com/personal/userregistration/business/dto/UserRegistrationDto.java
@@ -4,11 +4,13 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.*;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 import java.time.LocalDate;
 
 @Getter
 @Setter
+@ToString
 public class UserRegistrationDto {
 
     @Size(min = 2, max = 60)
