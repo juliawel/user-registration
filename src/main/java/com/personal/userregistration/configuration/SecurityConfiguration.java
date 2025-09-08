f07b5dfaba3c0626e35528149d44c83f24df1c10
Julia Welter <140450028+juliawel@users.noreply.github.com>
2025-09-08 11:14:30 -0300

    feat: Enable public access to API and H2 console

--- src/main/java/com/personal/userregistration/configuration/SecurityConfiguration.java
+++ src/main/java/com/personal/userregistration/configuration/SecurityConfiguration.java
@@ -2,11 +2,34 @@ package com.personal.userregistration.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable);
+
+        http.authorizeHttpRequests(authorize -> authorize
+                .anyRequest().permitAll()
+        );
+
+        http.headers(headers -> headers.frameOptions(frame -> frame.disable()));
+
+        http.httpBasic(withDefaults());
+
+        return http.build();
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
